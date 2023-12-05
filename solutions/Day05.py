5f6feb8901e5307e5f68f303736b6f35ab5baccb
luctia <timmerman.luc1999@gmail.com>
2023-12-05 15:43:38 +0100

    WIP Day 5 part 2

--- solutions/Day05.py
+++ solutions/Day05.py
@@ -3,7 +3,6 @@ from sys import maxsize
 
 
 def get_map(input_lines, type):
-    found = False
     i = 0
     while input_lines[i] != type:
         i += 1
@@ -25,43 +24,80 @@ def lookup_in_map(value, mapped):
     return None
 
 
+def get_seed_location(seed, input_lines):
+    seed_soil = get_map(input_lines, "seed-to-soil map:")
+    soil_fert = get_map(input_lines, "soil-to-fertilizer map:")
+    fert_wat = get_map(input_lines, "fertilizer-to-water map:")
+    wat_light = get_map(input_lines, "water-to-light map:")
+    light_temp = get_map(input_lines, "light-to-temperature map:")
+    temp_hum = get_map(input_lines, "temperature-to-humidity map:")
+    hum_loc = get_map(input_lines, "humidity-to-location map:")
+    return lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(seed, seed_soil), soil_fert), fert_wat), wat_light), light_temp), temp_hum), hum_loc)
+
+
 class Day05(Day):
     def part1(self):
         input_lines = self.get_input_lines()
         seeds = [int(seed) for seed in input_lines[0].split(" ")[1:]]
-        seed_soil = get_map(input_lines, "seed-to-soil map:")
-        soil_fert = get_map(input_lines, "soil-to-fertilizer map:")
-        fert_wat = get_map(input_lines, "fertilizer-to-water map:")
-        wat_light = get_map(input_lines, "water-to-light map:")
-        light_temp = get_map(input_lines, "light-to-temperature map:")
-        temp_hum = get_map(input_lines, "temperature-to-humidity map:")
-        hum_loc = get_map(input_lines, "humidity-to-location map:")
         lowest_loc = maxsize
         for seed in seeds:
-            location = lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(lookup_in_map(seed, seed_soil), soil_fert), fert_wat), wat_light), light_temp), temp_hum), hum_loc)
+            location = get_seed_location(seed, input_lines)
             if location is not None and location < lowest_loc:
                 lowest_loc = location
         self.answer(lowest_loc)
 
     def part2(self):
         input_lines = self.get_input_lines()
-        seed_ranges = [int(seed) for seed in input_lines[0].split(" ")[1:]]
-        seed_soil = get_map(input_lines, "seed-to-soil map:")
-        soil_fert = get_map(input_lines, "soil-to-fertilizer map:")
-        fert_wat = get_map(input_lines, "fertilizer-to-water map:")
-        wat_light = get_map(input_lines, "water-to-light map:")
-        light_temp = get_map(input_lines, "light-to-temperature map:")
-        temp_hum = get_map(input_lines, "temperature-to-humidity map:")
-        hum_loc = get_map(input_lines, "humidity-to-location map:")
+        seeds = [int(seed) for seed in input_lines[0].split(" ")[1:]]
+        seed_ranges = []
+        for i in range(len(seeds) - 1):
+            seed_ranges.append((seeds[i], seeds[i] + seeds[i+1] - 1))
 
-        lowest_dest = maxsize
+        # lowest_range = None
+        # lowest_found = maxsize
+        #
+        # for r in seed_ranges:
+        #     for num in range(r[0], r[1], (r[1] - r[0]) // 10):
+        #         loc = get_seed_location(num, input_lines)
+        #         if loc is not None and loc < lowest_found:
+        #             lowest_found = loc
+        #             lowest_range = r
+
         lowest_range = None
-        for x in hum_loc:
-            if x[0] < lowest_dest:
-                lowest_range = x
-        start = lowest_range[1]
-        end = start + lowest_range[2]
+        lowest_found = maxsize
+        i = 0
+        for r in seed_ranges:
+            i += 1
+            print("Considering range " + str(i))
+            print("  (lowest so far: " + str(lowest_found) + ")")
+            picks = [num for num in range(r[0], r[1] + 4, (r[1] - r[0]) // 4)]
+            for pick in range(0, len(picks) - 1):
+                print("    pick " + str(pick + 1) + " of 4")
+                rrange = (picks[pick], picks[pick + 1])
 
-        lowest_ranges = []
+                if get_seed_location(rrange[0], input_lines) is None:
+                    pass
+                if get_seed_location(rrange[1], input_lines) is None:
+                    while get_seed_location(rrange[1], input_lines) is None:
+                        rrange = rrange[0], rrange[1] - 100
 
-        self.answer(1)
+                diff = rrange[1] - rrange[0]
+                while diff > 1:
+                    left_guess_seed = rrange[0] + diff // 4
+                    right_guess_seed = rrange[0] + (diff // 4) * 3
+                    left_guess_location = get_seed_location(left_guess_seed, input_lines)
+                    while left_guess_location is None:
+                        left_guess_seed = left_guess_seed + 10000
+                        left_guess_location = get_seed_location(left_guess_seed, input_lines)
+                    right_guess_location = get_seed_location(right_guess_seed, input_lines)
+                    while right_guess_location is None:
+                        right_guess_seed = right_guess_seed - 10000
+                        right_guess_location = get_seed_location(right_guess_seed, input_lines)
+                    if left_guess_location < right_guess_location:
+                        rrange = rrange[0], rrange[0] + diff // 2
+                    else:
+                        rrange = rrange[0] + diff // 2, rrange[1]
+                    diff = rrange[1] - rrange[0]
+                lowest_found = min([lowest_found, left_guess_location, right_guess_location])
+
+        self.answer(lowest_found)
