62d088715327aac209c6602b5966e81caaacbfd8
Luc Timmerman <timmerman.luc1999@gmail.com>
2023-12-09 13:56:44 +0100

    Day 9 part 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from solutions import Day09
 
 day = Day09.Day09()
-day.run_all((None, None))
+day.run_all((114, 2))
--- solutions/Day09.py
+++ solutions/Day09.py
@@ -24,4 +24,21 @@ class Day09(Day):
         return total
 
     def part2(self, test=False):
-        pass
+        sequences = []
+        for line in self.get_input_lines("test" if test else ""):
+            sequences.append([int(x) for x in line.split(" ")])
+        total = 0
+        for sequence in sequences:
+            resulting_sequences = []
+            while not all([x == 0 for x in sequence[:-1]]):
+                resulting_sequences.append(sequence)
+                new_sequence = []
+                for i in range(1, len(sequence)):
+                    new_sequence.append(sequence[i] - sequence[i - 1])
+                sequence = new_sequence
+            resulting_sequences.append(sequence)
+            to_add = 0
+            for i in range(len(resulting_sequences) - 1, -1, -1):
+                to_add = resulting_sequences[i][0] - to_add
+            total += to_add
+        return total
